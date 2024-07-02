3f82a3366a663e19e3ca798992f95074446056b1
chmesser <chmesser@deloitte.com>
2024-07-02 15:08:19 -0500

    adding terraform config file

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    random = "~> 2.2"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "region" {
+  type = string
+  default = "eastus2"
+}
+
+
+data "azurerm_client_config" "current" {
+}
+
+data "external" "me" {
+  program = ["az", "account", "show", "--query", "user"]
+}
+
+locals {
+  prefix = "databricks-f1-tf"
+  tags = {
+    Environment = "Demo"
+    Owner       = lookup(data.external.me.result, "name")
+  }
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "${local.prefix}-rg"
+  location = var.region
+  tags     = local.tags
+}
+
+resource "azurerm_databricks_workspace" "this" {
+  name                        = "${local.prefix}-workspace"
+  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this.location
+  sku                         = "premium"
+  managed_resource_group_name = "${local.prefix}-workspace-rg"
+  tags                        = local.tags
+}
+
+output "databricks_host" {
+  value = "https://${azurerm_databricks_workspace.this.workspace_url}/"
+}
